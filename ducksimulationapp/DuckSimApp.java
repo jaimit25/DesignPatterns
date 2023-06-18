8b97835dce2be56ab976607de94a7c8e14f40c59
jaimit25 <jaimit.jp@gmail.com>
2023-06-18 22:08:31 +0530

    STRATEGY PATTERN | Sun Jun 18 22:08:31 IST 2023

--- ducksimulationapp/DuckSimApp.java
+++ ducksimulationapp/DuckSimApp.java
@@ -3,6 +3,8 @@ package ducksimulationapp;
 import ducksimulationapp.duckabstractclass.Duck;
 import ducksimulationapp.duckimplementationclass.MallardDuck;
 
+
+// STRATEGY PATTERN
 public class DuckSimApp {
     public static void main(String[] args) {
         Duck duck = new MallardDuck();
